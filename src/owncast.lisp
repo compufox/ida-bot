bf2a23263bcd3d455971b1faa988225239e3f554
a. fox <dev@computerfox.xyz>
2023-03-22 17:19:48 -0400

    added missing owncast integrations api requests

--- src/owncast.lisp
+++ src/owncast.lisp
@@ -15,38 +15,62 @@
    :moderate-user))
 (in-package :ida-bot.actions)
 
+(defun owncast-request (api-path &key payload (method :post) (content-type "application/json"))
+  "runs the owncast request"
+  (http-request (str:concat (env :stream-url) api-path)
+                :method method
+                :content-type content-type
+                :accept "application/json"
+                :content (when payload (stringify (alist-hash-table payload)))
+                :additional-headers `(("Authorization" . ,(str:concat "Bearer " (env :access-token))))))
 
+(defun set-stream-title (new-title)
+  (owncast-request "/api/integrations/streamtitle"
+                   :payload 
+                   `(("value" . ,new-title))))
+
+;; chat functions 
 (defun send-chat (message)
   "sends a chat message to the configured server"
   (owncast-request "/api/integrations/chat/send"
+                   :payload 
                    `(("body" . ,message))))
 
 (defun send-system-chat (message)
   (owncast-request "/api/integrations/chat/system"
+                   :payload 
                    `(("body" . ,message))))
+
+(defun send-chat-action (message &optional author)
+  (owncast-request "/api/integrations/chat/action"
+                   :payload
+                   (append `(("body" . ,message))
+                           (when author `(("author" . ,author))))))
 
 (defun send-system-chat-to-client (client-id message)
   (owncast-request (str:concat "/api/integrations/chat/system/client/" client-id)
+                   :payload 
                    `(("body" . ,message))))
 
+;; moderation functions
 (defun moderate-chat (message-id set-visible)
   "moderates a chat message with id MESSAGE-ID, setting it visible based on SET-VISIBLE"
   (owncast-request "/api/integrations/chat/messagevisibility"
+                   :payload 
                    `(("visible" . ,(when set-visible t))
                      ("idArray" . (,message-id)))))
-
+#|
+;; this uses a non-integration api method and as such doesnt work! :upside_down:
 (defun moderate-user (user-id set-enabled)
   "moderates a user with USER-ID, enabling them based on SET-ENABLED"
   (owncast-request "/api/chat/users/setenabled"
+                   :payload 
                    `(("userId" . ,user-id)
                      ("enabled" . ,(when set-enabled t)))))
+|#
 
-(defun owncast-request (api-path payload)
-  "runs the owncast request"
-  (http-request (str:concat (env :stream-url) api-path)
-                :method :post
-                :content-type "application/json"
-                :accept "application/json"
-                :content (stringify (alist-hash-table payload))
-                :additional-headers `(("Authorization" . ,(str:concat "Bearer " (env :access-token))))))
-          
+(defun get-connected-clients ()
+  (owncast-request "/api/integrations/clients" :method :get))
+
+(defun get-chat-backlog ()
+  (owncast-request "/api/integrations/chat" :method :get))
