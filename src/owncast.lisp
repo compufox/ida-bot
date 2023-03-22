13ec1cc24e3e22192891e04f8bc2fcb38c66d432
a. fox <dev@computerfox.xyz>
2023-03-22 10:10:31 -0400

    added system chat functions into owncast api package

--- src/owncast.lisp
+++ src/owncast.lisp
@@ -9,27 +9,37 @@
   (:export
 
    :send-chat
+   :send-system-chat
+   :send-system-chat-to-client
    :moderate-chat
    :moderate-user))
 (in-package :ida-bot.actions)
+
 
 (defun send-chat (message)
   "sends a chat message to the configured server"
   (owncast-request "/api/integrations/chat/send"
                    `(("body" . ,message))))
-  
+
+(defun send-system-chat (message)
+  (owncast-request "/api/integrations/chat/system"
+                   `(("body" . ,message))))
+
+(defun send-system-chat-to-client (client-id message)
+  (owncast-request (str:concat "/api/integrations/chat/system/client/" client-id)
+                   `(("body" . ,message))))
 
 (defun moderate-chat (message-id set-visible)
   "moderates a chat message with id MESSAGE-ID, setting it visible based on SET-VISIBLE"
   (owncast-request "/api/chat/messagevisibility"
-                   `(("visible" . ,set-visible)
+                   `(("visible" . ,(when set-visible t))
                      ("idArray" . (,message-id)))))
 
 (defun moderate-user (user-id set-enabled)
   "moderates a user with USER-ID, enabling them based on SET-ENABLED"
   (owncast-request "/api/chat/users/setenabled"
                    `(("userId" . ,user-id)
-                     ("enabled" . ,set-enabled))))
+                     ("enabled" . ,(when set-enabled t)))))
 
 (defun owncast-request (api-path payload)
   "runs the owncast request"
